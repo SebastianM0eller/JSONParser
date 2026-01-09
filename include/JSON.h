4ed8854bdc1beb6c754abd142e12f0182840528f
Sebastian Moeller <SebastianMOlesen@gmail.com>
2026-01-09 18:56:16 +0100

    Added the methods to get the data

--- include/JSON.h
+++ include/JSON.h
@@ -6,6 +6,7 @@
 #include <variant>
 #include <vector>
 #include <map>
+#include <stdexcept>
 #include <string>
 
 /**
@@ -33,4 +34,54 @@ struct JSONValue
   [[nodiscard]] bool IsString() const { return std::holds_alternative<std::string>(data); }
   [[nodiscard]] bool IsJSONArray() const { return std::holds_alternative<std::vector<JSONValue>>(data); }
   [[nodiscard]] bool IsJSONObject() const { return std::holds_alternative<std::map<std::string, JSONValue>>(data); }
+
+  // Accessing JSONArray
+  JSONValue& operator[](const unsigned int index)
+  {
+    if (!IsJSONArray()) throw std::runtime_error("Cannot access element of non-array JSON value");
+    return std::get<std::vector<JSONValue>>(data).at(index);
+  }
+
+  // Accessing JSONObject
+  JSONValue& operator[](const std::string& key)
+  {
+    if (!IsJSONObject()) throw std::runtime_error("Cannot access element of non-object JSON value");
+    return std::get<std::map<std::string, JSONValue>>(data).at(key);
+  }
+
+  // Handling the c-style string
+  JSONValue& operator[](const char* key)
+  {
+    return (*this)[std::string(key)];
+  }
+
+
+  // Implicit conversions for regular data
+  operator std::string() const
+  {
+    if (!IsString()) throw std::runtime_error("Cannot convert non-string JSON value to string");
+    return std::get<std::string>(data);
+  }
+
+  operator int() const
+  {
+    if (!IsInt()) throw std::runtime_error("Cannot convert non-integer JSON value to int");
+    return std::get<int>(data);
+  }
+
+  operator double() const
+  {
+    if (!IsDouble()) throw std::runtime_error("Cannot convert non-double JSON value to double");
+    return std::get<double>(data);
+  }
+
+  operator bool() const
+  {
+    if (!IsBool()) throw std::runtime_error("Cannot convert non-bool JSON value to bool");
+    return std::get<bool>(data);
+  }
+
+
+
+
 };
